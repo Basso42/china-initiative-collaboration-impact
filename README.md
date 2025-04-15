92f77192a141a0a1aa358fded39b71d49610ca82
gamer35 <florian.basso@ensae.fr>
2025-04-15 17:43:06 +0000

    fetching, cleaning, testing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # citations-network
 Network analysis of scientific citations based on the HALvest dataset
+
+To fetch the citation network dataset, run
+```
+init.sh
+```
+
--- init.sh
+++ init.sh
@@ -2,4 +2,12 @@ sudo apt update
 sudo apt install git-lfs
 
 git lfs install
-git clone https://huggingface.co/datasets/almanach/HALvest
+git clone https://huggingface.co/datasets/almanach/HALvest-Geometric
+
+current_path=$(pwd)
+mkdir data
+
+cd HALvest-Geometric
+unzip raw.zip -d "$current_path/data"
+
+rm -rf HALvest-Geometric
