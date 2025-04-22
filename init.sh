e2982a90923d8e6a94e0c493d2d1f71d47a247ee
gamer35 <florian.basso@ensae.fr>
2025-04-22 16:09:35 +0000

    updated rqts

--- init.sh
+++ init.sh
@@ -1,13 +1,1 @@
-sudo apt update
-sudo apt install git-lfs
-
-git lfs install
-git clone https://huggingface.co/datasets/almanach/HALvest-Geometric
-
-current_path=$(pwd)
-mkdir data
-
-cd HALvest-Geometric
-unzip raw.zip -d "$current_path/data"
-
-rm -rf HALvest-Geometric
+pip install -r requirements.txt
