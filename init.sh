4e25acb3c787168211c5fbcd005e8e7eaf2ce6da
gamer35 <florian.basso@ensae.fr>
2025-05-02 17:26:01 +0000

    added data pipeline

--- init.sh
+++ init.sh
@@ -1,1 +1,9 @@
+mkdir -p data
+if [ ! -f data/all_works_16_20.parquet ]; then
+    wget -O data/all_works_16_20.parquet https://minio.lab.sspcloud.fr/gamer35/public/all_works_16_20.parquet
+else
+    echo "File already exists, skipping download."
+fi
+
 pip install -r requirements.txt
+
